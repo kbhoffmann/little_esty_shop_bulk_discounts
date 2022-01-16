88cc461f2bbfdd705671accaecb1773596fc5111
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-01-16 13:02:19 -0700

    add additional test instances and framwork for tests needed for total revenue with and without discounts

--- README.md
+++ README.md
@@ -74,7 +74,7 @@ When I visit my bulk discount show page
 Then I see a link to edit the bulk discount
 When I click this link
 Then I am taken to a new page with a form to edit the discount
-[ ]
+[X]
 And I see that the discounts current attributes are pre-poluated in the form
 When I change any/all of the information and click submit
 Then I am redirected to the bulk discount's show page
--- spec/features/invoices/show_spec.rb
+++ spec/features/invoices/show_spec.rb
@@ -11,7 +11,6 @@ RSpec.describe 'invoices show' do
     @item_4 = Item.create!(name: "Hair tie", description: "This holds up your hair", unit_price: 1, merchant_id: @merchant1.id)
     @item_7 = Item.create!(name: "Scrunchie", description: "This holds up your hair but is bigger", unit_price: 3, merchant_id: @merchant1.id)
     @item_8 = Item.create!(name: "Butterfly Clip", description: "This holds up your hair but in a clip", unit_price: 5, merchant_id: @merchant1.id)
-
     @item_5 = Item.create!(name: "Bracelet", description: "Wrist bling", unit_price: 200, merchant_id: @merchant2.id)
     @item_6 = Item.create!(name: "Necklace", description: "Neck bling", unit_price: 300, merchant_id: @merchant2.id)
 
@@ -22,6 +21,17 @@ RSpec.describe 'invoices show' do
     @customer_5 = Customer.create!(first_name: 'Sylvester', last_name: 'Nader')
     @customer_6 = Customer.create!(first_name: 'Herber', last_name: 'Kuhn')
 
+    @transaction1 = Transaction.create!(credit_card_number: 203942, result: 1, invoice_id: @invoice_1.id)
+    @transaction2 = Transaction.create!(credit_card_number: 230948, result: 1, invoice_id: @invoice_2.id)
+    @transaction3 = Transaction.create!(credit_card_number: 234092, result: 1, invoice_id: @invoice_3.id)
+    @transaction4 = Transaction.create!(credit_card_number: 230429, result: 1, invoice_id: @invoice_4.id)
+    @transaction5 = Transaction.create!(credit_card_number: 102938, result: 1, invoice_id: @invoice_5.id)
+    @transaction6 = Transaction.create!(credit_card_number: 879799, result: 0, invoice_id: @invoice_6.id)
+    @transaction7 = Transaction.create!(credit_card_number: 203942, result: 1, invoice_id: @invoice_7.id)
+    @transaction8 = Transaction.create!(credit_card_number: 203942, result: 1, invoice_id: @invoice_8.id)
+    ################
+    @transaction11 = Transaction.create!(credit_card_number: 203942, result: 1, invoice_id: @invoice_11.id)
+
     @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 2, created_at: "2012-03-27 14:54:09")
     @invoice_2 = Invoice.create!(customer_id: @customer_1.id, status: 2, created_at: "2012-03-28 14:54:09")
     @invoice_3 = Invoice.create!(customer_id: @customer_2.id, status: 2)
@@ -29,8 +39,9 @@ RSpec.describe 'invoices show' do
     @invoice_5 = Invoice.create!(customer_id: @customer_4.id, status: 2)
     @invoice_6 = Invoice.create!(customer_id: @customer_5.id, status: 2)
     @invoice_7 = Invoice.create!(customer_id: @customer_6.id, status: 2)
-
     @invoice_8 = Invoice.create!(customer_id: @customer_6.id, status: 1)
+    #################
+    @invoice_11 = Invoice.craete!(customer_id: @customer_1.id, status: 2)
 
     @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 9, unit_price: 10, status: 2)
     @ii_2 = InvoiceItem.create!(invoice_id: @invoice_2.id, item_id: @item_1.id, quantity: 1, unit_price: 10, status: 2)
@@ -42,15 +53,14 @@ RSpec.describe 'invoices show' do
     @ii_9 = InvoiceItem.create!(invoice_id: @invoice_7.id, item_id: @item_4.id, quantity: 1, unit_price: 1, status: 1)
     @ii_10 = InvoiceItem.create!(invoice_id: @invoice_8.id, item_id: @item_5.id, quantity: 1, unit_price: 1, status: 1)
     @ii_11 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_8.id, quantity: 12, unit_price: 6, status: 1)
+    ###############
+    @ii_20 = InvoiceItem.create!(invoice_id: @invoice_11.id, item_id: @item_1.id, quantity: 10, unit_price: 10, status: 2)
+    @ii_21 = InvoiceItem.create!(invoice_id: @invoice_11.id, item_id: @item_2.id, quantity: 20, unit_price: 8, status: 2)
+    @ii_22 = InvoiceItem.create!(invoice_id: @invoice_11.id, item_id: @item_1.id, quantity: 30, unit_price: 5, status: 2)
 
-    @transaction1 = Transaction.create!(credit_card_number: 203942, result: 1, invoice_id: @invoice_1.id)
-    @transaction2 = Transaction.create!(credit_card_number: 230948, result: 1, invoice_id: @invoice_2.id)
-    @transaction3 = Transaction.create!(credit_card_number: 234092, result: 1, invoice_id: @invoice_3.id)
-    @transaction4 = Transaction.create!(credit_card_number: 230429, result: 1, invoice_id: @invoice_4.id)
-    @transaction5 = Transaction.create!(credit_card_number: 102938, result: 1, invoice_id: @invoice_5.id)
-    @transaction6 = Transaction.create!(credit_card_number: 879799, result: 0, invoice_id: @invoice_6.id)
-    @transaction7 = Transaction.create!(credit_card_number: 203942, result: 1, invoice_id: @invoice_7.id)
-    @transaction8 = Transaction.create!(credit_card_number: 203942, result: 1, invoice_id: @invoice_8.id)
+    @discount1 = Discount.create!(percentage_discount: 10, quantity_threshold: 10, merchant_id: @merchant1.id)
+    @discount2 = Discount.create!(percentage_discount: 20, quantity_threshold: 20, merchant_id: @merchant1.id)
+    @discount3 = Discount.create!(percentage_discount: 30, quantity_threshold: 30, merchant_id: @merchant1.id)
   end
 
   it "shows the invoice information" do
@@ -100,4 +110,11 @@ RSpec.describe 'invoices show' do
      end
   end
 
+  xit 'shows the total revenue for the merchant without discounts' do
+    visit merchant_invoice_path(@merchant1, @invoice_20)
+  end
+
+  xit 'shows the total discounted revenue for the merchant WITH discounts' do
+    visit merchant_invoice_path(@merchant1, @invoice_20)
+  end
 end
