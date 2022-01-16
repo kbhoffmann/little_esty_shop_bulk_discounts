a559f1e9c860ba966120c2d19a212afbfc8173f6
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-01-15 19:03:13 -0700

    edit test and model validation to validate that integer is greater than 0

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,3 +1,5 @@
 class Discount < ApplicationRecord
   belongs_to :merchant
+  validates :percentage_discount, presence: true, numericality: {greater_than: 0}
+  validates :quantity_threshold, presence: true, numericality: {greater_than: 0}
 end
--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Discount do
   describe 'validations' do
     it { should validate_presence_of :percentage_discount}
     it { should validate_presence_of :quantity_threshold}
-    it { should validate_numericality_of(:percentage_discount)}
-    it { should validate_numericality_of(:quantity_threshold)}
+    it { should validate_numericality_of(:percentage_discount).is_greater_than(0)}
+    it { should validate_numericality_of(:quantity_threshold).is_greater_than(0)}
   end
 end
