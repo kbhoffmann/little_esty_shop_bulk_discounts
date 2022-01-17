4b62303982b9387fb8575df1ff6a4edd29dc4b3d
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-01-17 07:12:34 -0700

    add test for finding the revenue that is eligible for the discount

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -26,7 +26,7 @@ RSpec.describe Invoice, type: :model do
   end
 
   describe 'Bulk Discounts Revenue' do
-    it 'discounts a single item that meets the quantity threshold' do
+    it 'finds the max percentage discount by quantity' do
       merchant1 = Merchant.create!(name: 'Hair Care')
       customer1 = Customer.create!(first_name: 'Brooke', last_name: 'Stewart')
 
@@ -69,8 +69,47 @@ RSpec.describe Invoice, type: :model do
       expect(invoice3.max_percent_discount).to eq(10)
       expect(invoice4.max_percent_discount).to eq(nil)
     end
+
+    it 'finds revenue that is discountable' do
+      merchant1 = Merchant.create!(name: 'Hair Care')
+      customer1 = Customer.create!(first_name: 'Brooke', last_name: 'Stewart')
+
+      invoice1 = Invoice.create!(customer_id: customer1.id, status: 2)
+      invoice2 = Invoice.create!(customer_id: customer1.id, status: 2)
+      invoice3 = Invoice.create!(customer_id: customer1.id, status: 2)
+      invoice4 = Invoice.create!(customer_id: customer1.id, status: 2)
+
+      transaction1 = Transaction.create!(credit_card_number: 203942, result: 1, invoice_id: invoice1.id)
+      transaction2 = Transaction.create!(credit_card_number: 203942, result: 1, invoice_id: invoice2.id)
+      transaction3 = Transaction.create!(credit_card_number: 203942, result: 1, invoice_id: invoice2.id)
+      transaction4 = Transaction.create!(credit_card_number: 203942, result: 1, invoice_id: invoice2.id)
+
+      item1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: merchant1.id, status: 1)
+      item2 = Item.create!(name: "Conditioner", description: "This makes your hair shiny", unit_price: 8, merchant_id: merchant1.id, status: 1)
+      item3 = Item.create!(name: "Brush", description: "This takes out tangles", unit_price: 5, merchant_id: merchant1.id, status: 1)
+      #All items eligible for bulk discount
+      ii1 = InvoiceItem.create!(invoice_id: invoice1.id, item_id: item1.id, quantity: 10, unit_price: 10, status: 2)
+      ii2 = InvoiceItem.create!(invoice_id: invoice1.id, item_id: item2.id, quantity: 10, unit_price: 8, status: 2)
+      ii3 = InvoiceItem.create!(invoice_id: invoice1.id, item_id: item3.id, quantity: 10, unit_price: 5, status: 2)
+      #2 items elibigle for bulk discount
+      ii4 = InvoiceItem.create!(invoice_id: invoice2.id, item_id: item1.id, quantity: 10, unit_price: 10, status: 2)
+      ii5 = InvoiceItem.create!(invoice_id: invoice2.id, item_id: item2.id, quantity: 10, unit_price: 8, status: 2)
+      ii6 = InvoiceItem.create!(invoice_id: invoice2.id, item_id: item3.id, quantity: 5, unit_price: 5, status: 2)
+      #1 item eligible for bulk discount
+      ii7 = InvoiceItem.create!(invoice_id: invoice3.id, item_id: item1.id, quantity: 10, unit_price: 10, status: 2)
+      ii8 = InvoiceItem.create!(invoice_id: invoice3.id, item_id: item2.id, quantity: 5, unit_price: 8, status: 2)
+      ii9 = InvoiceItem.create!(invoice_id: invoice3.id, item_id: item3.id, quantity: 5, unit_price: 5, status: 2)
+      #no items elibigle for bulk discount == even need this?????? May be accounted for in max percentage discount
+      ii10 = InvoiceItem.create!(invoice_id: invoice4.id, item_id: item1.id, quantity: 5, unit_price: 10, status: 2)
+      ii11 = InvoiceItem.create!(invoice_id: invoice4.id, item_id: item2.id, quantity: 5, unit_price: 8, status: 2)
+      ii12 = InvoiceItem.create!(invoice_id: invoice4.id, item_id: item3.id, quantity: 5, unit_price: 5, status: 2)
+
+      discount1 = Discount.create!(percentage_discount: 10, quantity_threshold: 10, merchant_id: merchant1.id)
+
+      expect(invoice1.discountable_revenue).to eq(230)
+      expect(invoice2.discountable_revenue).to eq(180)
+      expect(invoice3.discountable_revenue).to eq(100)
+      expect(invoice4.discountable_revenue).to eq(0)
+    end
   end
 end
-
-# expect(invoice1.revenue_after_discount).to eq(235)
-#break down problem and find max percentage_discount
