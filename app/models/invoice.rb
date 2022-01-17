9682c4da76d523421af305c5040348da439944bb
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-01-16 20:16:06 -0700

    add pseudocode and 2 possible new methods

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -22,4 +22,21 @@ class Invoice < ApplicationRecord
           .pluck('discounts.percentage_discount').last
           #later call .max??????
   end
+
+  def qualified_items
+    #items in the invoice that qualify for the discount
+    #Items .where('invoice_items.quantity >= discounts.quantity_threshold')
+    #write lots of tests
+  end
+
+  def unqualified_items
+    #items in the invoice that don't qualitfy for the discount
+    #Items .where('invoice_items.quantity < discounts.quantity_threshold')
+    #write lots of tests
+  end
+
+  # qualified_items_revenue * max_percent_discount
+  # unqualified_items_revenue
+  # add the two together to get the discounted revenue
+  # may need if statements
 end
