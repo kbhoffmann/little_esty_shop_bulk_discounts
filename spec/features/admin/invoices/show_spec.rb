187b83fee37ad47c8b35975f6fa4f5b469ec8448
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-01-17 17:45:37 -0700

    test: add test for showing total and discounted revenue on admin invoice show page

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -1,23 +1,25 @@
 require 'rails_helper'
 
 describe 'Admin Invoices Index Page' do
-  before :each do
-    @m1 = Merchant.create!(name: 'Merchant 1')
+  describe 'legacy code' do
+    before :each do
+      @m1 = Merchant.create!(name: 'Merchant 1')
 
-    @c1 = Customer.create!(first_name: 'Yo', last_name: 'Yoz', address: '123 Heyyo', city: 'Whoville', state: 'CO', zip: 12345)
-    @c2 = Customer.create!(first_name: 'Hey', last_name: 'Heyz')
+      @c1 = Customer.create!(first_name: 'Yo', last_name: 'Yoz', address: '123 Heyyo', city: 'Whoville', state: 'CO', zip: 12345)
+      @c2 = Customer.create!(first_name: 'Hey', last_name: 'Heyz')
 
-    @i1 = Invoice.create!(customer_id: @c1.id, status: 2, created_at: '2012-03-25 09:54:09')
-    @i2 = Invoice.create!(customer_id: @c2.id, status: 1, created_at: '2012-03-25 09:30:09')
+      @i1 = Invoice.create!(customer_id: @c1.id, status: 2, created_at: '2012-03-25 09:54:09')
+      @i2 = Invoice.create!(customer_id: @c2.id, status: 1, created_at: '2012-03-25 09:30:09')
 
-    @item_1 = Item.create!(name: 'test', description: 'lalala', unit_price: 6, merchant_id: @m1.id)
-    @item_2 = Item.create!(name: 'rest', description: 'dont test me', unit_price: 12, merchant_id: @m1.id)
+      @item_1 = Item.create!(name: 'test', description: 'lalala', unit_price: 6, merchant_id: @m1.id)
+      @item_2 = Item.create!(name: 'rest', description: 'dont test me', unit_price: 12, merchant_id: @m1.id)
 
-    @ii_1 = InvoiceItem.create!(invoice_id: @i1.id, item_id: @item_1.id, quantity: 12, unit_price: 2, status: 0)
-    @ii_2 = InvoiceItem.create!(invoice_id: @i1.id, item_id: @item_2.id, quantity: 6, unit_price: 1, status: 1)
-    @ii_3 = InvoiceItem.create!(invoice_id: @i2.id, item_id: @item_2.id, quantity: 87, unit_price: 12, status: 2)
+      @ii_1 = InvoiceItem.create!(invoice_id: @i1.id, item_id: @item_1.id, quantity: 12, unit_price: 2, status: 0)
+      @ii_2 = InvoiceItem.create!(invoice_id: @i1.id, item_id: @item_2.id, quantity: 6, unit_price: 1, status: 1)
+      @ii_3 = InvoiceItem.create!(invoice_id: @i2.id, item_id: @item_2.id, quantity: 87, unit_price: 12, status: 2)
 
-    visit admin_invoice_path(@i1)
+      visit admin_invoice_path(@i1)
+    end
   end
 
   it 'should display the id, status and created_at' do
@@ -67,6 +69,49 @@ describe 'Admin Invoices Index Page' do
 
       expect(current_path).to eq(admin_invoice_path(@i1))
       expect(@i1.status).to eq('complete')
+    end
+  end
+
+  describe 'Bulk Discounts Project User Story 9' do
+    xit 'shows total revenue from invoice WITHOUT discounts' do
+      merchant1 = Merchant.create!(name: 'Hair Care')
+      customer1 = Customer.create!(first_name: 'Brooke', last_name: 'Stewart')
+
+      invoice1 = Invoice.create!(customer_id: customer1.id, status: 2)
+      transaction1 = Transaction.create!(credit_card_number: 203942, result: 1, invoice_id: invoice1.id)
+
+      item1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: merchant1.id, status: 1)
+      item2 = Item.create!(name: "Conditioner", description: "This makes your hair shiny", unit_price: 8, merchant_id: merchant1.id)
+      item3 = Item.create!(name: "Brush", description: "This takes out tangles", unit_price: 5, merchant_id: merchant1.id)
+
+      ii1 = InvoiceItem.create!(invoice_id: invoice1.id, item_id: item1.id, quantity: 5, unit_price: 10, status: 2)
+      ii2 = InvoiceItem.create!(invoice_id: invoice1.id, item_id: item2.id, quantity: 5, unit_price: 8, status: 2)
+      ii3 = InvoiceItem.create!(invoice_id: invoice1.id, item_id: item3.id, quantity: 10, unit_price: 5, status: 2)
+
+      visit admin_invoice_path(invoice1)
+
+      expect(page).to have_content("Total Revenue: $140.00")
+    end
+
+    xit 'shows the total DISCOUNTED revenue from invoice' do
+      merchant1 = Merchant.create!(name: 'Hair Care')
+      customer1 = Customer.create!(first_name: 'Brooke', last_name: 'Stewart')
+
+      invoice1 = Invoice.create!(customer_id: customer1.id, status: 2)
+      transaction1 = Transaction.create!(credit_card_number: 203942, result: 1, invoice_id: invoice1.id)
+
+      item1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: merchant1.id, status: 1)
+      item2 = Item.create!(name: "Conditioner", description: "This makes your hair shiny", unit_price: 8, merchant_id: merchant1.id)
+      item3 = Item.create!(name: "Brush", description: "This takes out tangles", unit_price: 5, merchant_id: merchant1.id)
+
+      ii1 = InvoiceItem.create!(invoice_id: invoice1.id, item_id: item1.id, quantity: 5, unit_price: 10, status: 2)
+      ii2 = InvoiceItem.create!(invoice_id: invoice1.id, item_id: item2.id, quantity: 5, unit_price: 8, status: 2)
+      ii3 = InvoiceItem.create!(invoice_id: invoice1.id, item_id: item3.id, quantity: 10, unit_price: 5, status: 2)
+      discount1 = Discount.create!(percentage_discount: 10, quantity_threshold: 10, merchant_id: merchant1.id)
+
+      visit admin_invoice_path(invoice1)
+
+      expect(page).to have_content("Revenue after discount: $135.00")
     end
   end
 end
