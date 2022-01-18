bfdc9dc809436f2dbb2409c22773f132720ca63e
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-01-18 11:46:33 -0700

    add test for validating percentage discount cannot be greater than 100

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,6 +1,7 @@
 class Discount < ApplicationRecord
   belongs_to :merchant
   validates :percentage_discount, presence: true, numericality: {greater_than: 0}
+  validates_numericality_of :percentage_discount, less_than_or_equal_to: 100
   validates :quantity_threshold, presence: true, numericality: {greater_than: 0}
   validates_numericality_of :quantity_threshold, only_integer: true
 end
