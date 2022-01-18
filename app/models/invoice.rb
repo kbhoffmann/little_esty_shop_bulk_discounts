cb76ce6d3fcfa027cf71c1431c364370681af01e
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-01-18 11:22:32 -0700

    unskip tests and fix errors; delete unneeded tests and methods in invoice

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -14,29 +14,6 @@ class Invoice < ApplicationRecord
     invoice_items.sum("unit_price * quantity")
   end
 
-  # def max_percent_discount
-  #   invoice_items.joins(item: {merchant: :discounts})
-  #         .where('invoice_items.quantity >= discounts.quantity_threshold')
-  #         .select('discounts.percentage_discount, invoice_items.*')
-  #         .order('discounts.percentage_discount')
-  #         .pluck('discounts.percentage_discount').last
-  #         #later call .max??????
-  # end
-
-  # def discountable_revenue
-  #   invoice_items.joins(item: {merchant: :discounts})
-  #         .where("invoice_items.quantity >= discounts.quantity_threshold")
-  #         .select("invoice_items.unit_price * invoice_items.quantity")
-  #         .sum("invoice_items.unit_price * invoice_items.quantity")
-  # end
-
-  # def non_discountable_revenue
-  #   invoice_items.joins(item: {merchant: :discounts})
-  #         .where("invoice_items.quantity < discounts.quantity_threshold")
-  #         .select("invoice_items.unit_price * invoice_items.quantity")
-  #         .sum("invoice_items.unit_price * invoice_items.quantity")
-  # end
-
   def revenue_after_discount
     invoice_items.sum do |invoice_item|
       invoice_item.discounted_revenue
--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -20,57 +20,57 @@ describe 'Admin Invoices Index Page' do
 
       visit admin_invoice_path(@i1)
     end
-  end
 
-  xit 'should display the id, status and created_at' do
-    expect(page).to have_content("Invoice ##{@i1.id}")
-    expect(page).to have_content("Created on: #{@i1.created_at.strftime("%A, %B %d, %Y")}")
+    it 'should display the id, status and created_at' do
+      expect(page).to have_content("Invoice ##{@i1.id}")
+      expect(page).to have_content("Created on: #{@i1.created_at.strftime("%A, %B %d, %Y")}")
 
-    expect(page).to_not have_content("Invoice ##{@i2.id}")
-  end
-
-  xit 'should display the customers name and shipping address' do
-    expect(page).to have_content("#{@c1.first_name} #{@c1.last_name}")
-    expect(page).to have_content(@c1.address)
-    expect(page).to have_content("#{@c1.city}, #{@c1.state} #{@c1.zip}")
-
-    expect(page).to_not have_content("#{@c2.first_name} #{@c2.last_name}")
-  end
-
-  xit 'should display all the items on the invoice' do
-    expect(page).to have_content(@item_1.name)
-    expect(page).to have_content(@item_2.name)
-
-    expect(page).to have_content(@ii_1.quantity)
-    expect(page).to have_content(@ii_2.quantity)
-
-    expect(page).to have_content("$#{@ii_1.unit_price}")
-    expect(page).to have_content("$#{@ii_2.unit_price}")
-
-    expect(page).to have_content(@ii_1.status)
-    expect(page).to have_content(@ii_2.status)
-
-    expect(page).to_not have_content(@ii_3.quantity)
-    expect(page).to_not have_content("$#{@ii_3.unit_price}")
-    expect(page).to_not have_content(@ii_3.status)
-  end
-
-  xit 'should display the total revenue the invoice will generate' do
-    expect(page).to have_content("Total Revenue: $#{@i1.total_revenue}")
-
-    expect(page).to_not have_content(@i2.total_revenue)
-  end
-
-  xit 'should have status as a select field that updates the invoices status' do
-    within("#status-update-#{@i1.id}") do
-      select('cancelled', :from => 'invoice[status]')
-      expect(page).to have_button('Update Invoice')
-      click_button 'Update Invoice'
-
-      expect(current_path).to eq(admin_invoice_path(@i1))
-      expect(@i1.status).to eq('complete')
+      expect(page).to_not have_content("Invoice ##{@i2.id}")
     end
-  end
+
+    it 'should display the customers name and shipping address' do
+      expect(page).to have_content("#{@c1.first_name} #{@c1.last_name}")
+      expect(page).to have_content(@c1.address)
+      expect(page).to have_content("#{@c1.city}, #{@c1.state} #{@c1.zip}")
+
+      expect(page).to_not have_content("#{@c2.first_name} #{@c2.last_name}")
+    end
+
+    it 'should display all the items on the invoice' do
+      expect(page).to have_content(@item_1.name)
+      expect(page).to have_content(@item_2.name)
+
+      expect(page).to have_content(@ii_1.quantity)
+      expect(page).to have_content(@ii_2.quantity)
+
+      expect(page).to have_content("$#{@ii_1.unit_price}")
+      expect(page).to have_content("$#{@ii_2.unit_price}")
+
+      expect(page).to have_content(@ii_1.status)
+      expect(page).to have_content(@ii_2.status)
+
+      expect(page).to_not have_content(@ii_3.quantity)
+      expect(page).to_not have_content("$#{@ii_3.unit_price}")
+      expect(page).to_not have_content(@ii_3.status)
+    end
+
+    it 'should display the total revenue the invoice will generate' do
+      expect(page).to have_content("Total Revenue: $#{@i1.total_revenue}")
+
+      expect(page).to_not have_content(@i2.total_revenue)
+    end
+
+    it 'should have status as a select field that updates the invoices status' do
+      within("#status-update-#{@i1.id}") do
+        select('cancelled', :from => 'invoice[status]')
+        expect(page).to have_button('Update Invoice')
+        click_button 'Update Invoice'
+
+        expect(current_path).to eq(admin_invoice_path(@i1))
+        expect(@i1.status).to eq('complete')
+      end
+    end
+end
 
   describe 'Bulk Discounts Project User Story 9' do
     it 'shows total revenue from invoice WITHOUT discounts' do
